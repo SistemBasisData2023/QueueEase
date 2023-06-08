bc079b57fe8036bdac5fb833077722f9e18c7244
IdleGoat <rafie.amandio@ui.ac.id>
2023-06-08 14:12:39 +0700

    Live-Server Integration

--- Frontend/src/components/Home.tsx
+++ Frontend/src/components/Home.tsx
@@ -2,12 +2,8 @@ function Home() {
 
     return (
         <>
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <div className="card-body">
-                <video width="300%" height="300%" controls>
-                    <source src="https://www.youtube.com/watch?v=0uGETVnkujA" type="video/mp4" />
-                </video>
-            </div>
+        <div className="mockup-window border bg-base-300">
+            <div className="flex justify-center px-4 py-16 bg-base-200">Hello!</div>
         </div>
         </>
     )
