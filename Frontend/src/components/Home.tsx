5bc72ca30e84031eda109427ece4b8a4e6e195da
IdleGoat <rafie.amandio@ui.ac.id>
2023-06-09 15:32:35 +0700

    Loading Login, Main Queue

--- Frontend/src/components/Home.tsx
+++ Frontend/src/components/Home.tsx
@@ -1,6 +1,38 @@
+import React, { useState, useEffect } from 'react';
 import ReactPlayer from 'react-player';
+import axios from 'axios';
+import { API_ENDPOINT } from '../config/config';
+
+interface QueueItem {
+  desk_id: number;
+  highest_queue_id: number;
+}
 
 function Home() {
+  const [queue, setQueue] = useState<QueueItem[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${API_ENDPOINT}/queue/getByTeller`);
+        setQueue(response.data);
+      } catch (error) {
+        console.log('Error fetching data:', error);
+      }
+    };
+
+    const intervalId = setInterval(fetchData, 2000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const getQueueByDeskId = (deskId: number) => {
+    const desk = queue.find((item) => item.desk_id === deskId);
+    return desk ? desk.highest_queue_id : 0;
+  };
+
   return (
     <>
       <div className="p-6">
@@ -8,7 +40,7 @@ function Home() {
           <div className="flex flex-col items-center justify-center">
             <ReactPlayer
               className="w-2/3 h-auto"
-              url="src\assets\video\home_video.mp4"
+              url="src/assets/video/home_video.mp4"
               playing
               loop
               muted
@@ -16,8 +48,38 @@ function Home() {
             />
           </div>
         </div>
+
+        <div className="grid grid-cols-4 gap-4 mt-6">
+          <QueueBox title="Loket 1" queue={getQueueByDeskId(1)} />
+          <QueueBox title="Loket 2" queue={getQueueByDeskId(2)} />
+          <QueueBox title="Loket 3" queue={getQueueByDeskId(3)} />
+          <QueueBox title="Loket 4" queue={getQueueByDeskId(4)} />
+        </div>
+
+        <div className="mt-6">
+          <h2 className="text-xl font-bold mb-2">Queue List</h2>
+          <ul>
+            {queue.map((item, index) => (
+              <li key={index}>{item.highest_queue_id}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
+  );
+}
+
+interface QueueBoxProps {
+  title: string;
+  queue: number;
+}
+
+function QueueBox({ title, queue }: QueueBoxProps) {
+  return (
+    <div className="bg-base-200 p-4 rounded-xl">
+      <h2 className="text-lg font-semibold mb-2">{title}</h2>
+      <p className="text-gray-500">Queue: {queue}</p>
+    </div>
   );
 }
 
