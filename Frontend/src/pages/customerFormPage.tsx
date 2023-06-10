00dab08efb87472b5bf12ba1fcc68109c44768d2
laurenchristyt <lauren.christy@ui.ac.id>
2023-06-10 21:17:32 +0700

    Fixed Merge

--- Frontend/src/pages/customerFormPage.tsx
+++ Frontend/src/pages/customerFormPage.tsx
@@ -1,17 +1,5 @@
 import CustomerForm from '../components/customerForm/customerForm';
 
-<<<<<<< HEAD
-
-
-function CustomerFormPage() {
-    return (
-        <>
-          <CustomerForm />
-    
-        </>
-      );
-    }
-=======
 function CustomerFormPage() {
   return (
     <>
@@ -19,6 +7,6 @@ function CustomerFormPage() {
     </>
   );
 }
->>>>>>> a42a1b40af63fc2bb28e4714d646af8a2dbe97fd
+
 
 export default CustomerFormPage;
